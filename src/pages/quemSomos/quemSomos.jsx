d75c4480924c93fb7727661226ed30a8421c4841
PietroPonciano <pietro.ponciano2023@gmail.com>
2026-08-12 16:53:06 -0300

    Melhorando a base do projeto

---
+++ src/pages/quemSomos/quemSomos.jsx
@@ -0,0 +1,7 @@
+export default function QuemSomos(){
+    return(
+        <>
+        <h1>Quem Somos</h1>
+        </>
+    )
+}
---
+++ src/pages/solucoes/solucoes.jsx
@@ -0,0 +1,7 @@
+export default function Solucoes(){
+    return(
+        <>
+        <h1>Soluções</h1>
+        </>
+    )
+}
