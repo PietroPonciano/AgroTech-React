844375b4262c68cfaed049b805fccf69fb3e6579
PietroPonciano <pietro.ponciano2023@gmail.com>
2026-09-03 17:24:21 -0300

    Resolvendo bug do react router

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router";
+import { BrowserRouter } from "react-router-dom";
 
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap-icons/font/bootstrap-icons.css";
